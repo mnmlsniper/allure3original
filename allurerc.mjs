b66bf0c7baf7a7b51f633849e7940ab3f5ec4a73
Dmitry Baev <baev.dm@gmail.com>
2024-12-17 16:32:22 +0000

    improve report generation (via #27)

--- allurerc.mjs
+++ allurerc.mjs
@@ -1,16 +1,13 @@
 import { defineConfig } from "allure";
 
-// TODO: add history path
 export default defineConfig({
   name: "Allure Report 3",
   output: "./out/allure-report",
   plugins: {
     awesome: {
       options: {
-        reportName: "Allure Report 3",
         singleFile: true,
         reportLanguage: "en",
-        open: true,
       },
     },
     log: {
